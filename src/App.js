409c72816c090e4c195d53e6364cc966cef20282
Irina <irbris@mail.ru>
2019-08-19 15:10:20 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import './App.css';
+import WeatherDisplay from './components/WeatherDisplay/WeatherDisplay';
+
+const PLACES = ["London", "Moscow", "Kiev", "Minsk"];
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0,
+    };
+  }
+  
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+      <div className="App">        
+      
+        {PLACES.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({ activePlace: index });
+            }}
+          >
+              {place}
+          </button>
+        ))}
+
+        <WeatherDisplay 
+          key={activePlace}
+          name={PLACES[activePlace]} />
+      </div>
+    );
+  }
+}
